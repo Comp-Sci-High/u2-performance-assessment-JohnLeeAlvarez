316fbc6398d911c7f2c0baa709bde6e0481c0ec9
John Lee <109625459+JohnLeeAlvarez@users.noreply.github.com>
2024-11-19 14:39:32 +0000

    Not complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+const prompt = require('prompt-sync')()
+
+async function generateImage(recipesdata) {
+
+    const apiKey = "7be7a7ae601149c68935b7b7e7d48e10"
+    const url = "https://api.spoonacular.com/recipes/random?apiKey=7be7a7ae601149c68935b7b7e7d48e10"
+  
+
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ` + apiKey,
+        'Content-Type': 'application/json'
+      }
+    }
+const response = await fetch(url, options)
+const data = await response.json()
+console.log(data)
+return data
+
+  }
+
+  let question = prompt("Would you like a random recipes?")
+
+  generateImage(question)
